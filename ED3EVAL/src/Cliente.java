775b3b3e19a757eec373d2e555ab6a42e71bf0eb
sag10 <sag10@DESKTOP-O8HFHU4>
2024-05-30 17:24:58 +0200

    Nueva clase Cliente en rama cliente

---
+++ ED3EVAL/src/Cliente.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+public class Cliente {
+ private String nombre;
+ private String direccion;
+ private String telefono;
+ private double saldo;
+ private ArrayList<Product> productos;
+ public Cliente(String nombre, String direccion, String telefono, double saldo) {
+ this.nombre = nombre;
+ this.direccion = direccion;
+ this.telefono = telefono;
+ this.saldo = saldo;
+ this.productos = new ArrayList<>();
+}
+ public void agregarProducto(Product producto) {
+ productos.add(producto);
+ }
+ public double calcularPrecioTotalProductos() {
+ double precioTotal = 0;
+ for (Product producto : productos) {
+ precioTotal += producto.getPrice();
+ }
+ return precioTotal;
+ }
+ public void restarPrecioProductosAlSaldo() {
+ double precioTotalProductos = calcularPrecioTotalProductos();
+ saldo -= precioTotalProductos;
+ }
+}
